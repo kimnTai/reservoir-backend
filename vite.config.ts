4e7fe367d4765d0a7fedaeab81679625c462701a
kimn <a9523023650@gmail.com>
2023-02-27 23:24:21 +0800

    [初始化]

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import { VitePluginNode } from "vite-plugin-node";
+
+export default defineConfig({
+    plugins: [
+        ...VitePluginNode({
+            adapter: "express",
+            appPath: "./src/app.ts",
+        }),
+    ],
+});
